c35d76ec988588a2e7643c8f835abd89278d5010
Salo <salofc.omg@gmail.com>
2024-05-22 14:05:45 -0500

    API v1.0.1

--- build/app.js
+++ build/app.js
@@ -11,18 +11,39 @@ var _initialSetup = require("./libs/initialSetup.js");
 var _products = _interopRequireDefault(require("./routes/products.routes"));
 var _auth = _interopRequireDefault(require("./routes/auth.routes"));
 var _user = _interopRequireDefault(require("./routes/user.routes"));
+var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
+
+// Inicializar roles
 (0, _initialSetup.createRoles)();
-var path = require('path');
+
+// Configurar variables de la aplicación
 app.set('pkg', _package["default"]);
-app.use((0, _morgan["default"])("dev"));
-app.use(_express["default"]["static"](path.join(__dirname, 'public')));
+
+// Middlewares
+app.use((0, _morgan["default"])('dev'));
 app.use(_express["default"].json());
+
+// Configurar carpeta de archivos estáticos
+var staticPath = _path["default"].join(__dirname, '..', 'public');
+app.use(_express["default"]["static"](staticPath));
+
+// Rutas
+app.use('/api/products', _products["default"]);
+app.use('/api/auth', _auth["default"]);
+app.use('/api/user', _user["default"]);
+
+// Ruta raíz para manejar solicitudes a "/"
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(_path["default"].join(staticPath, 'index.html'));
 });
-app.use('/api/products', _products["default"]); // products
-app.use('/api/auth', _auth["default"]); // auth
-app.use('/api/user', _user["default"]); // user
+
+// Obtener el puerto del entorno o usar el puerto 5000 por defecto
+var port = process.env.PORT || 5000;
+
+// Iniciar el servidor
+app.listen(port, function () {
+  console.log("Server listening on port: ".concat(port));
+});
 var _default = exports["default"] = app;
--- src/app.js
+++ src/app.js
@@ -1,31 +1,44 @@
 import express from 'express';
 import morgan from 'morgan';
-import pkg from '../package.json'
-import {createRoles} from './libs/initialSetup.js';
-import productRoutes from './routes/products.routes'
-import authRoutes from './routes/auth.routes'
-import userRoutes from './routes/user.routes'
+import pkg from '../package.json';
+import { createRoles } from './libs/initialSetup.js';
+import productRoutes from './routes/products.routes';
+import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/user.routes';
+import path from 'path';
 
+const app = express();
 
-const app = express()
+// Inicializar roles
+createRoles();
 
-createRoles()
-
-const path = require('path');
-
-
+// Configurar variables de la aplicación
 app.set('pkg', pkg);
-app.use(morgan("dev"))
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
 
+// Configurar carpeta de archivos estáticos
+const staticPath = path.join(__dirname, '..', 'public');
+app.use(express.static(staticPath));
+
+// Rutas
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+// Ruta raíz para manejar solicitudes a "/"
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(staticPath, 'index.html'));
 });
 
-app.use('/api/products', productRoutes); // products
-app.use('/api/auth', authRoutes); // auth
-app.use('/api/user', userRoutes); // user
+// Obtener el puerto del entorno o usar el puerto 5000 por defecto
+const port = process.env.PORT || 5000;
 
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
 
 export default app;
