b273dd9ec208fd4de2b1174d1f17e3f3f6dff3b2
Salo <salofc.omg@gmail.com>
2024-05-28 16:04:12 -0500

    API v1.0.15

--- build/config.js
+++ build/config.js
@@ -8,3 +8,4 @@ var _default = exports["default"] = {
   SECRET: 'A8f3$7dKz!Lp',
   MONGO_CONNECT: 'mongodb+srv://saloAdminUDEC:jgqEvvNm87KrRXNM@cluster0.fdr4fxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }; //  MONGO_LOCAL: mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6
+// pass MONGO_CONNECT: jgqEvvNm87KrRXN (Si falla la conecxíon, revisar las ip de acceso en Mongo Atlas)
