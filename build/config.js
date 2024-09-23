18b64fe58abd43326679242e9306e720a7d657cb
Salomón Forero <94926757+salo-0mg@users.noreply.github.com>
2024-09-23 09:27:00 -0500

    Update config.js

--- build/config.js
+++ build/config.js
@@ -5,7 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _default = exports["default"] = {
-  SECRET: 'A8f3$7dKz!Lp',
-  MONGO_CONNECT: 'mongodb+srv://saloAdminUDEC:jgqEvvNm87KrRXNM@cluster0.fdr4fxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-}; //  MONGO_LOCAL: mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6
-// pass MONGO_CONNECT: jgqEvvNm87KrRXN (Si falla la conecxíon, revisar las ip de acceso en Mongo Atlas)
+  SECRET: 'Aquí va la palabra secreta',
+  MONGO_CONNECT: 'Aquí va la dirección a la base de datos'
+}; 
