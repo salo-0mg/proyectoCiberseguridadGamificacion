4e079d51f13ae23c7324b514c78bbb3b5dedca4e
Salomón Forero <94926757+salo-0mg@users.noreply.github.com>
2024-09-23 09:40:30 -0500

    Update config.js

--- build/config.js
+++ build/config.js
@@ -5,6 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var _default = exports["default"] = {
-  SECRET: 'Aquí va la palabra secreta',
-  MONGO_CONNECT: 'Aquí va la dirección a la base de datos'
+  SECRET: 'A8f3$7dKz!Lp',
+  MONGO_CONNECT: 'mongodb+srv://saloAdminUDEC:jgqEvvNm87KrRXNM@cluster0.fdr4fxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }; 
